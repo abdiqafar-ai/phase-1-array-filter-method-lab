6a4ded9784a9edc328da69e42b8bc47980ccdceb
abdiqafar <iabdiqafar24@gmail.com>
2024-10-01 09:25:50 +0300

    commit

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution here
+// Code your solution here  
+function findMatching(drivers, query) {
+    let lowercaseQuery = query.toLowerCase();
+
+    let matchingDrivers = drivers.filter(driver =>
+        driver.toLowerCase() === lowercaseQuery
+    );
+
+    return matchingDrivers;
+}
+
+function fuzzyMatch(drivers, query) {
+    let lowercaseQuery = query.toLowerCase();
+
+    let matchingDrivers = drivers.filter(driver =>
+        driver.toLowerCase().startsWith(lowercaseQuery)
+    );
+
+    return matchingDrivers;
+}
+function matchName(drivers, query) {
+    let lowercaseQuery = query.toLowerCase();
+
+    let matchingDrivers = drivers.filter(driver =>
+        driver.name.toLowerCase() === lowercaseQuery
+    );
+ return matchingDrivers;
+}
